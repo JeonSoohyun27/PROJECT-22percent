de2c908d5115518b3796a1813545b3a2b7d891ad
Dongmyeong Lee <dom9316@gmail.com>
2021-07-21 10:47:37 +0900

    Add: user validator

---
+++ users/utils.py
@@ -0,0 +1,31 @@
+import jwt
+
+from django.http    import JsonResponse
+
+from users.models   import User
+from my_settings    import SECRET_KEY, ALGORITHM
+
+def user_validator(function):
+    def wrapper(self, request, *args, **kwargs):
+        try:
+            acces_token = request.headers.get('Authorization', None)
+
+            if not acces_token:
+                return JsonResponse({"message": "NEED_LOGIN"}, status=401)
+
+            payload      = jwt.decode(acces_token, SECRET_KEY, ALGORITHM)
+            user         = User.objects.get(id=payload["user_id"])
+            request.user = user
+
+            return function(self, request, *args, **kwargs)
+        
+        except jwt.DecodeError:
+            return JsonResponse({"message": "INVALID_TOKEN"}, status=401)
+
+        except jwt.ExpiredSignatureError:
+            return JsonResponse({"message": "EXPIRED_TOKEN"}, status=401)
+
+        except User.DoesNotExist:
+            return JsonResponse({"message": "INVALID_USER"}, status=401)
+
+    return wrapper
