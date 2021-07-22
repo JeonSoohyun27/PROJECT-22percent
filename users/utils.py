b973c81ee41038659bdfb1ca4ef381c45008456a
Dongmyeong Lee <dom9316@gmail.com>
2021-07-22 17:16:32 +0900

    Add: Migration files

--- users/utils.py
+++ users/utils.py
@@ -1,6 +1,8 @@
+import uuid
 import jwt
 
 from django.http      import JsonResponse
+from django.db.models import Q
 
 from users.models   import User
 from my_settings    import SECRET_KEY, ALGORITHM
@@ -29,3 +31,11 @@ def user_validator(function):
             return JsonResponse({"message": "INVALID_USER"}, status=401)
 
     return wrapper
+
+def create_random_account():
+    random_account = str(uuid.uuid4().int>>64)[0:17]
+    
+    if User.objects.filter(Q(deposit_account=random_account) | Q(withdrawal_account=random_account)).exists():
+        return create_random_account()
+
+    return random_account
