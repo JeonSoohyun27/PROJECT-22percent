44f91fdeae29e29a4b56b53c4f614053f8bf637c
Dongmyeong Lee <dom9316@gmail.com>
2021-07-28 17:52:33 +0900

    Add: public login

--- users/utils.py
+++ users/utils.py
@@ -32,6 +32,30 @@ def user_validator(function):
 
     return wrapper
 
+def public_login(function):
+    def wrapper(self, request, *args, **kwargs):
+        try:
+            access_token = request.headers.get('Authorization', None)
+            
+            request.user = None
+            if access_token:
+                payload      = jwt.decode(access_token, SECRET_KEY, ALGORITHM)
+                user         = User.objects.get(id=payload["user_id"])
+                request.user = user
+            
+            return function(self, request, *args, **kwargs)
+
+        except jwt.DecodeError:
+            return JsonResponse({"message": "INVALID_TOKEN"}, status=401)
+
+        except jwt.ExpiredSignatureError:
+            return JsonResponse({"message": "EXPIRED_TOKEN"}, status=401)
+
+        except User.DoesNotExist:
+            return JsonResponse({"message": "INVALID_USER"}, status=401)
+
+    return wrapper
+
 def create_random_account():
     random_account = str(uuid.uuid4().int>>64)[0:17]
     
